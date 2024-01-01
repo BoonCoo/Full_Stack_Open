1f4097c433f072b1004a59506adfa5c57258c32b
BoonCoo <146928217+BoonCoo@users.noreply.github.com>
2024-01-01 01:43:42 +0000

    Part 2 exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,4 +1,23 @@
-import Course from "./components/Course"
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map((part) => <Part key={part.id} part={part}/>)}      
+  </>
+
+const Course = ({ course }) => 
+  <>
+  <Header course={course.name} />
+  <Content parts={course.parts} />
+  <Total sum={course.parts.map((part) => part.exercises).reduce((tally,a) => tally + a,0)} />
+  </>
 
 const App = () => {
   const course = {
