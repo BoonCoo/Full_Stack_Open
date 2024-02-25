38417c89a96f8e06a9193856460d92615d918897
BoonCoo <146928217+BoonCoo@users.noreply.github.com>
2024-02-25 23:22:13 +0000

    Phonebook

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -2,23 +2,45 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+
   ]) 
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+      id: persons.length + 1,
+    }
+  
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input id='name' autoComplete='on' value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => 
+          <li key = {person.id}>
+            {person.content}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
