f934462ae241473ef58d51f822b7f40066cfc21c
BoonCoo <146928217+BoonCoo@users.noreply.github.com>
2024-03-10 17:13:12 +0000

    Phonebook 2.9

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -15,9 +14,11 @@ const App = () => {
       const nameObject = {
         content: newName,
         id: persons.length + 1,
+        number: newNumber
       }
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
 
   }
@@ -27,22 +28,24 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input id='name' autoComplete='on' value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div> name: <input id='name' autoComplete='on' value={newName} onChange={handleNameChange}/> </div>
+        <div> number: <input id='number' autoComplete='on' value={newNumber} onChange={handleNumberChange}/> </div>
+        <div> <button type="submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => 
           <li key = {person.id}>
-            {person.content}
+            {person.content} {person.number}
           </li>
         )}
       </ul>
