98d5a8590127f48a119d9d3f6a6064db3f1aeb57
BoonCoo <146928217+BoonCoo@users.noreply.github.com>
2024-03-18 17:37:21 +0000

    Phonebook

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -9,6 +9,18 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  // const [newList, setNewList] = useState('')
+
+  const addFilter = (event) => {
+    event.preventDefault()
+    // const list = result.filter((name) => name.toLowerCase().includes(newFilter.toLowerCase()))
+    setNewFilter('')
+    // setNewList(persons.map(person => person.name).filter((name) => name.toLowerCase().includes(newFilter.toLowerCase())))
+  }
+
+  const filteredpersons = persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+  console.log(filteredpersons)
 
   const addName = (event) => {
     event.preventDefault()
@@ -27,6 +39,12 @@ const App = () => {
     }
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+    // console.log(newFilter)
+  }
+
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
@@ -40,6 +58,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={addFilter}>
+        <div> filter shown with <input id='filter' autoComplete='on' value={newFilter} onChange={handleFilterChange}/> </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div> name: <input id='name' autoComplete='on' value={newName} onChange={handleNameChange}/> </div>
         <div> number: <input id='number' autoComplete='on' value={newNumber} onChange={handleNumberChange}/> </div>
@@ -47,7 +69,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {filteredpersons.map(person => 
           <li key = {person.id}>
             {person.name} {person.number}
           </li>
